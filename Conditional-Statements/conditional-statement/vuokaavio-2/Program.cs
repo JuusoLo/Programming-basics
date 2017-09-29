d1456dfe0e5078281d3a148dfe7e199f1ac77e58
JuusoLo <lonka.juuso@gmail.com>
2017-09-29 10:40:48 +0300

    vuokaavio-2 update

---
+++ Conditional-Statements/conditional-statement/vuokaavio-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace vuokaavio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = 0;
+            int price = 16;
+            bool discounted = false;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Defain instructions
+            Console.WriteLine("Ohjelma kertoo lipun hinnan.");
+            {
+                Console.Write("Syötä ikä");
+            }
+
+
+
+            
+        }
+    }
+}
