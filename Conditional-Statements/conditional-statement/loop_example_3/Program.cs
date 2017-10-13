98b2c163f0c3ec38b7e9528d7989e066aa9c1399
JuusoLo <lonka.juuso@gmail.com>
2017-10-13 10:26:55 +0300

    update vuokaavio 3

--- Conditional-Statements/conditional-statement/loop_example_3/Program.cs
+++ Conditional-Statements/conditional-statement/loop_example_3/Program.cs
@@ -6,7 +6,29 @@ namespace loop_example_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee. Anna luku, mistä parillisten ja parittomien summan:");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            int parillinen = 0;
+            int pariton = 0;
+            while (i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    parillinen = parillinen + i;
+                }
+                else
+                {
+                    pariton = pariton + i;
+                }
+                i = i + 1;
+            }
+
+            Console.WriteLine($"Parillisten ja parittomien lukujen summa: {parillinen} ja {pariton}");
+            Console.ReadKey();
         }
     }
 }
