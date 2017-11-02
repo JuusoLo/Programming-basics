53e6660789705470face7e92a7b1e90db980165f
JuusoLo <lonka.juuso@gmail.com>
2017-11-02 13:33:22 +0200

    Update

---
+++ arrays/Taulukot/Taulukot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Taulukot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma arpoo 1000 lukua väliltä 0-50 ja muodostaa niistä talukon.");
+            int[] arrayNumber = new int[1000];
+            Random rnd = new Random();
+            double sum = 0;
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                arrayNumber[i] = rnd.Next(0, 51);
+            }
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                Console.WriteLine($"{i}. {arrayNumber[i]}");
+                sum += arrayNumber[i]; 
+            }
+            Console.WriteLine($"Lukujen summa on: {sum}");
+            Console.WriteLine($"Lukujen keskiarvo on: {sum / arrayNumber.Length}");
+            Console.ReadKey();
+
+
+
+        }
+
+
+    }
+}
