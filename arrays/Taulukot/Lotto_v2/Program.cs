251d0eccb0e9f1c75d3205fd52f8cf369ba05fe4
JuusoLo <lonka.juuso@gmail.com>
2017-11-09 09:49:15 +0200

    update

---
+++ arrays/Taulukot/Lotto_v2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lotto_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int[] arrayNumber = new int[40];
+            Random rnd = new Random();
+            int i = 0;
+            //Arvotaan seitsemän eri lukua
+            while (i < 7)
+            {
+                int rndNumber = rnd.Next(0, 40);
+                if (arrayNumber[rndNumber] == 0)
+                {
+                    arrayNumber[rndNumber] = 1;
+                    i++;
+                }
+
+            }
+            for (i = 0; i < arrayNumber.Length; i++)
+            {
+                if (arrayNumber[i] == 1)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+            while (true)
+            {
+                int rndNumber = rnd.Next(0, 40);
+                if (arrayNumber[rndNumber] == 0)
+                {
+                    arrayNumber[rndNumber] = 2;
+                    break;
+
+                }
+            }
+            for (i = 0; i < arrayNumber.Length; i++)
+            {
+                if (arrayNumber[i] == 2)
+                {
+                    Console.WriteLine($"Lisänumero:{i}");
+                }
+            }
+            Console.Write($"Tuplausnumero:{rnd.Next(0, 40)}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ arrays/Taulukot/Taulukot_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Taulukot_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Random rnd = new Random();
+            //Taulukon pohjustamien, 7 päänumeroa, lisänumero ja tuplausnumero
+            int[] lotto = new int[9];
+
+            // Viimenen luku jää arpomatta, koska tuplausnumero voi olla sama kuin jo arvottu luku
+            for (int i = 0; i < lotto.Length; i++)
+            {
+                //luku väliltä 1-40
+                lotto[i] = rnd.Next(1, 41);
+
+                //Onko luku jo aijemmin arvottu
+                for (int j = 0; j < i; i++)
+                {
+                    if (lotto[j] == lotto[i])
+                    {
+                        i--;
+                    }
+                }
+            }
+            //Arvotaan tuplausnumero
+            lotto[8] = rnd.Next(1, 41);
+
+            //Järjestetään pienimmästä suurimpaan
+            Array.Sort(lotto, null, 0, 7);
+
+            //Tulostetaan luvut 1-7
+            for (int i = 0; i < lotto.Length - 2; i++)
+            {
+                Console.WriteLine($"{i + 1}. : {lotto[i]}");
+            }
+            Console.WriteLine($"Lisänumero: {lotto[7]}\nTuplausnumero: {lotto[8]}");
+            Console.ReadKey();
+        }
+    }
+}
