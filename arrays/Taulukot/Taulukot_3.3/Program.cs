d35e85506bc84b45eb403093e3145bdb48dd782a
JuusoLo <lonka.juuso@gmail.com>
2017-11-17 14:31:28 +0200

    Update

--- arrays/Taulukot/Taulukot_3.3/Program.cs
+++ arrays/Taulukot/Taulukot_3.3/Program.cs
@@ -7,19 +7,66 @@ namespace Taulukot_3._3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Ohjema arpoo yksiulotteiseen kokonaisluku talukkoon arvot väliltä 0-20");
-            int[,,] arrayNumber = new int[10,10,10];
+            Console.WriteLine("Ohjema määrittelee kolme kokonaislukutalukkoa arrT_1,arrT_2,arrT_3, joista jokaisessa on 10 kokonaislukua.");
             Random rnd = new Random();
-            Console.WriteLine("[arrT_1,arrT_2,arrT_3] = Arvo");
+            int[] arrT_1 = new int[10];
+            int[] arrT_2 = new int[10];
+            int[] arrT_3 = new int[10];
+            Console.WriteLine("arrT_1,arrT_2,arrT_3 = Arvo");
 
             for (int i = 0; i < 10; i++)
             {
-                for (int y = 0; y < 10; y++)
+                arrT_1[i] = rnd.Next(51);
+                arrT_2[i] = rnd.Next(51);
+
+                if (arrT_1[i] > arrT_2[i])
                 {
-                   
+                    arrT_3[i] = arrT_1[i];
                 }
-               
+                else
+                {
+                    arrT_3[i] = arrT_2[i];
+                }
             }
+
+            Console.Write("         ");
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 10)
+                {
+                    Console.Write($"0{i} ");
+                }
+                else
+                {
+                    Console.Write($"{i}\n");
+                }
+            }
+            Console.Write("         ");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("--");
+            }
+            Console.Write("\n arrT_1: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{arrT_1[i]:00} ");
+            }
+            Console.Write("\n arrT_2: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{arrT_2[i]:00} ");
+            }
+            Console.Write("\n arrT_3: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{arrT_3[i]:00} ");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
