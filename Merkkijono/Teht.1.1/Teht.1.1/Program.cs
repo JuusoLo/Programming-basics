0c2ee0419416b1cdce0f0e7b8363dc933bf4c9d0
JuusoLo <lonka.juuso@gmail.com>
2017-11-23 08:33:58 +0200

    Update

---
+++ Merkkijono/Teht.1.1/Teht.1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teht._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
