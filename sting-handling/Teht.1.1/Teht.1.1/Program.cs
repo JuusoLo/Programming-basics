6c796dcc3b69692fa97537763ae8d71ecbf7979a
JuusoLo <lonka.juuso@gmail.com>
2017-11-23 08:54:20 +0200

    Update

---
+++ sting-handling/Teht.1.1/Teht.1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Teht._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma ilmoittaa syöttämän virkkeesi merkkien määrän, syötä virke:");
+        }
+    }
+}
