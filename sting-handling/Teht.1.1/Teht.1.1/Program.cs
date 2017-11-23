63813e6cce6cdb10f79fb16e4cf4efb95067b9c4
JuusoLo <lonka.juuso@gmail.com>
2017-11-23 09:09:40 +0200

    Update

--- sting-handling/Teht.1.1/Teht.1.1/Program.cs
+++ sting-handling/Teht.1.1/Teht.1.1/Program.cs
@@ -8,6 +8,10 @@ namespace Teht._1._1
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma ilmoittaa syöttämän virkkeesi merkkien määrän, syötä virke:");
-        }
+            string merkkijono = Console.ReadLine();
+            int length = merkkijono.Length;
+            Console.WriteLine($"Syötteesä on {length} merkkiä");
+            Console.ReadKey();
+        }        
     }
 }
