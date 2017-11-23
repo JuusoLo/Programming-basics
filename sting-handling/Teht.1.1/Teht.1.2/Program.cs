faa1ffc4ae274e2d88758b1e5ff9e6a6bc59ebba
JuusoLo <lonka.juuso@gmail.com>
2017-11-23 09:38:45 +0200

    Update

---
+++ sting-handling/Teht.1.1/Teht.1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Teht._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma muuttaa E-kirjaimet @-merkeiksi, syötä virke: ");
+            string E = Console.ReadLine();           
+            E = E.Replace('e','@');
+            Console.WriteLine($"Loppullinen virkkeesi on: '{E}'");
+            Console.ReadKey();
+        }
+    }
+}
