d450ef3076b551fae047f97ac4d233b12c3a893a
JuusoLo <lonka.juuso@gmail.com>
2017-11-24 11:30:36 +0200

    update

---
+++ sting-handling/Teht.1.1/Teht.1.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Teht._1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Syötä virke, ohjelma laskee M-kirjaimien määrän:");
+            string UserInput = Console.ReadLine().ToUpper();
+
+            int i = 0;
+            foreach(char x in UserInput)
+            {
+                if (x == 'M')
+                    i++;
+            }
+            Console.WriteLine($"Tekstissä on {i} M-kirjainta! ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sting-handling/Teht.1.1/Teht.1.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Teht._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tarkistaa onko syöttämäsi teksti palindromi");
+            string UserInput = Console.ReadLine().ToUpper();
+            string reverseUserInput = "";
+
+            for (int i = UserInput.Length - 1; i >= 0; i--)
+            {
+                reverseUserInput += UserInput[i];
+            }
+            if (UserInput.Replace(" ", "") == reverseUserInput.Replace(" ", ""))
+            {
+                Console.WriteLine($"Syöttämäsi teksti,{UserInput.ToLower()} on palidromi");
+            }
+            else
+            {
+                Console.WriteLine($"Syöttämäsi teksti,{UserInput.ToLower()} ei ole palidromi!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
