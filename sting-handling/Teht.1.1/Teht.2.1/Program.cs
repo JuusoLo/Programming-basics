33456e368364a551602923d2aeb2b386bc05e03f
JuusoLo <lonka.juuso@gmail.com>
2017-11-30 11:26:28 +0200

    Update

---
+++ sting-handling/Teht.1.1/Teht.2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Teht._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee syöttämäsi virkkeesi vokaalien määrän.");
+            Console.WriteLine("Syötä virke:  ");
+            string userInput = Console.ReadLine().ToLower();
+            int MP = userInput.Length;
+
+            int count = 0;
+
+            foreach(char ch in userInput)
+            {
+                if(ch =='a' || ch =='e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y' || ch == 'ä' || ch == 'ö')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Virkkeesä: {userInput}, on vokaaleja: {count}" );
+
+            Console.ReadKey();
+        }
+    }
+}
